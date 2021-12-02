713cc27a3e0bd310202cd556a6ce8a3e7cddf090
Muhammad Hamid Raza <68273560+hamidrazabutt@users.noreply.github.com>
2021-12-03 03:25:55 +0500

    bootcamp2020-class5

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Parent from './Parent';
+
+function App() {
+  return (
+    <div>
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
